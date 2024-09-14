281f4680f83e80f6733b504e5ee50d758efb1269
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-09-14 10:38:40 +0700

    Update Packa ge

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['mhowwpetxddomyk9.public.blob.vercel-storage.com', 'images.unsplash.com']
+        domains: ['mhowwpetxddomyk9.public.blob.vercel-storage.com', 'images.unsplash.com','images.remotePatterns']
     }
 };
 
