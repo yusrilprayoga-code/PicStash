55d49c7d2cf6b6332d5e51d0acff07d50dd082db
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-09-19 15:57:11 +0700

    Feat: Update Dashboard

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { ImagesSlider } from "@/components/ui/image-slider";
+import { Link } from "lucide-react";
 
 export function HeroSection() {
   const images = [
@@ -28,10 +29,6 @@ export function HeroSection() {
         <motion.p className="font-bold text-xl md:text-5xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
             Upload and share your images <br /> with the world
         </motion.p>
-        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-        </button>
       </motion.div>
     </ImagesSlider>
   );
